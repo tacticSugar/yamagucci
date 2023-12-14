57bcc328e1f3d770552a321d2acc7ac761e42448
5417 <5417@tower.corp>
2023-12-14 16:54:44 +0300

    feat: noindex

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,7 +4,26 @@ import { FC } from 'react'
 /** документ */
 const Document: FC = () => (
   <Html lang='ru'>
-    <Head />
+    <Head>
+      <meta
+        content='nositelinkssearchbox'
+        key='sitelinks'
+        name='google'
+      />
+      <meta
+        content='notranslate'
+        key='notranslate'
+        name='google'
+      />
+      <meta
+        content='noindex,nofollow'
+        name='googlebot'
+      />
+      <meta
+        content='noindex,nofollow'
+        name='robots'
+      />
+    </Head>
     <body >
       <Main />
       <NextScript />
