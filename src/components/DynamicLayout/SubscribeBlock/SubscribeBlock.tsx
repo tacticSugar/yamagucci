176e44da2287ce1d504847b0a4b5b84c24253cf3
Timur Bykov (4538) <bikov.timur.013@gmail.com>
2023-12-19 10:40:04 +0300

    footer

--- src/components/DynamicLayout/SubscribeBlock/SubscribeBlock.tsx
+++ src/components/DynamicLayout/SubscribeBlock/SubscribeBlock.tsx
@@ -4,7 +4,8 @@ import styles from './SubscribeBlock.module.scss';
 
 const SubscribeBlock: FC = () => {
     return (
-        <div className={styles.footer}>
+        <div className={styles.subscribeBlock}>
+            <span className={styles.subscribeBlock__title}>Узнавайте об&nbsp;акциях первым!</span>
 
         </div>
     );
