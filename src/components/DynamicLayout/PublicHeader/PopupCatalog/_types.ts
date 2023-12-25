6953f8f2ed2a03d331d9176afdc34161024e4024
5070 <5070@tower.corp>
2023-12-25 16:34:07 +0300

    chore: добавил type SubcategoryArrayTypes

--- src/components/DynamicLayout/PublicHeader/PopupCatalog/_types.ts
+++ src/components/DynamicLayout/PublicHeader/PopupCatalog/_types.ts
@@ -1,3 +1,12 @@
+type SubcategoryArrayTypes = {
+  /** ссылка на страницу категории */
+  href: string
+  /** id  строки , для key */
+  id: number
+  /** название строки */
+  name: string
+}
+
 export type GlobalCategoryArrayTypes = {
   /** ссылка на страницу категории */
   href: string
@@ -7,4 +16,7 @@ export type GlobalCategoryArrayTypes = {
   img: string
   /** название строки */
   name: string
+
+  /** массив подкатегорий  */
+  subcategories: Array<SubcategoryArrayTypes>
 }
