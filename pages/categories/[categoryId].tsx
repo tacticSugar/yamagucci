34c66be43f71a66c79d40b562fae32a210a4059c
5417 <5417@tower.corp>
2023-12-26 16:25:31 +0300

    fix: minor fix

--- pages/categories/[categoryId].tsx
+++ pages/categories/[categoryId].tsx
@@ -2,9 +2,5 @@ import { memo } from 'react'
 
 import PublicCategoryPage from '@/src/components/pages/PublicCategoryPage/PublicCategoryPage'
 
-export const getStaticPaths = async (): Promise<any> => ({
-  fallback: 'blocking',
-  paths: []
-})
 // ts-prune-ignore-next
 export default memo(PublicCategoryPage)
